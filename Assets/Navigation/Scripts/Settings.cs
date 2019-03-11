040f6cd01d352972da63950dadac6dc84f64baf5
LukasMachala <mac0348@vsb.cz>
2019-03-11 22:18:15 +0100

    Přidáno GUI s nastavením scény a funkce skrýt nastavení

---
+++ Assets/Navigation/Scripts/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour {
+    public void OnHideSettingsClick()
+    {
+        gameObject.SetActive(false);
+
+        Debug.Log("Setting menu was hidden!");
+    }
+}
