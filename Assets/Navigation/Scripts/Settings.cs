6ed62ccd3c4a65505aec0f0e85b05b6295ec52b2
LukasMachala <mac0348@vsb.cz>
2019-03-12 18:56:29 +0100

    Přidán TapToPlace na GUI a nastaveno pořadí camera renderování pro viditelnost GUI

--- Assets/Navigation/Scripts/Settings.cs
+++ Assets/Navigation/Scripts/Settings.cs
@@ -1,12 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using HoloToolkit.Unity;
+using HoloToolkit.Unity.SpatialMapping;
+
 
 public class Settings : MonoBehaviour {
+    private TapToPlace tapToPlaceScript;
+    private Interpolator interpolatorScript;
+
+    private void Awake()
+    {
+        tapToPlaceScript = GetComponent<TapToPlace>();
+        interpolatorScript = GetComponent<Interpolator>();
+    }
+
     public void OnHideSettingsClick()
     {
         gameObject.SetActive(false);
-
+        
         Debug.Log("Setting menu was hidden!");
     }
 }
