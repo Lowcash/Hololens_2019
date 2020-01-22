3822ecbcc023e6c4040833de1a6b6c364091b7e1
LukasMachala <mac0348@vsb.cz>
2020-01-22 16:31:00 +0100

    Přidáno editační tlačítko ke stickeru

---
+++ Assets/Navigation/Scripts/Sticker.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sticker : MonoBehaviour {
+    public Text outputText;
+
+    public GameObject keyboardObject;
+
+    public void OnEditButtonClick() {
+    }
+}
